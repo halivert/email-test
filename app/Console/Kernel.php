32bdf855fb1ba308fb8f0258fda4bcdd9dbec86b
Halí V <halivelazquez@outlook.com>
2021-01-05 12:46:19 -0600

    Force fresh migration schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
 	protected function schedule(Schedule $schedule)
 	{
 		// $schedule->command('inspire')->hourly();
-		$schedule->command('migrate:fresh --step')->hourly();
+		$schedule->command('migrate:fresh --force --step')->hourly();
 	}
 
 	/**
